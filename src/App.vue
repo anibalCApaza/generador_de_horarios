2dd3547cc264640cb4fe24d5447b77f4a3fb67d1
arca fedora <arcacorini@gmail.com>
2025-02-12 13:25:25 -0400

    Implementación inicial

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="flex justify-center w-full h-screen">
+    <div class="text-2xl font-bold">
+      Un comodo generador de horarios
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
